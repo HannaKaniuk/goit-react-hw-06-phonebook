83161ff340e11fe70ee1d2c7b993ca85bae5c69e
HannaKaniuk <136392947+HannaKaniuk@users.noreply.github.com>
2024-01-21 22:03:07 +0200

    hw-react-6

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -38,7 +38,7 @@ export const ContactForm = () => {
       return;
     }
 
-    dispatch(addContact(name, number));
+    dispatch(addContact({ name, number }));
     setName('');
     setNumber('');
   };
